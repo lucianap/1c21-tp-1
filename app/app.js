f6e37e983e08493bc8419c9edfc1540ef341e7c8
Rodrigo Cotarelo <cotarelorodrigo@gmail.com>
2021-05-18 19:56:30 -0300

    bbox redirect by node app

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 
 const express = require('express')
+const request = require('request')
 const sleep = require('sleep');
 const app = express()
 const port = 3000
@@ -11,9 +12,24 @@ app.get('/node', (req, res) => {
 
   sleep.msleep(delay)
 
-  res.send('Hello World!')
+  res.send('Hello World Node!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+app.get('/bbox1', (req, res) => {
+  request.get('http://box:9090/').on('response', (response) => {
+    console.log("bbox1 " + response.statusCode)
+  })
+  res.send("bbox1")
+})
+
+app.get('/bbox2', (req, res) => {
+  request.get('http://box:9091/').on('response', (response) => {
+    console.log("bbox2 " + response.statusCode)
+  })
+  res.send("bbox2")
 })
