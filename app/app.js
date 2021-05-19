d348908fa4e209c071b83a3f08b7aefbe5df92d2
Nicolás Longo <longo.gnr@hotmail.com>
2021-05-18 21:23:33 -0300

    agrego endpoints propuestos por la cátedra

--- app/app.js
+++ app/app.js
@@ -4,12 +4,12 @@ const request = require('request')
 const sleep = require('sleep');
 const app = express()
 const port = 3000
+const TIMEOUT = 5*1000
 
-app.get('/node', (req, res) => {
+app.get('/node', (_req, res) => {
 
   // sleep random amount of time to test graphs
   var delay = (Math.floor(Math.random() * 500)) + 200
-
   sleep.msleep(delay)
 
   res.send('Hello World Node!')
@@ -20,16 +20,27 @@ app.listen(port, () => {
 })
 
 
-app.get('/bbox1', (req, res) => {
+app.get('/bbox1', (_req, res) => {
   request.get('http://box:9090/').on('response', (response) => {
     console.log("bbox1 " + response.statusCode)
   })
   res.send("bbox1")
 })
 
-app.get('/bbox2', (req, res) => {
+app.get('/bbox2', (_req, res) => {
   request.get('http://box:9091/').on('response', (response) => {
     console.log("bbox2 " + response.statusCode)
   })
   res.send("bbox2")
 })
+
+app.get('/ping', (_req, res) => {
+  res.status(200).send('Ping');
+})
+
+app.get('/heavy', (_req, res) => {
+  // Date() me devuelve milisegundos
+  let start = new Date();
+  while(new Date() - start < TIMEOUT);
+  res.status(200).send('Heavy work done')
+});
