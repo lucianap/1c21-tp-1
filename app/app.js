42d9bba02a22044e87a2d25faedbf9c36a59fdde
Nicolás Longo <longo.gnr@hotmail.com>
2021-05-23 19:58:01 -0300

    fix a endpoints y algunos scenarios

--- app/app.js
+++ app/app.js
@@ -12,35 +12,35 @@ app.get('/node', (_req, res) => {
   var delay = (Math.floor(Math.random() * 500)) + 200
   sleep.msleep(delay)
 
-  res.send('Hello World Node!')
-})
+  res.send('Hello World Node!\n')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
 
 
 app.get('/bbox1', (_req, res) => {
   request.get('http://box:9090/').on('response', (response) => {
     console.log("bbox1 " + response.statusCode)
+    res.send("bbox1 " + response.statusCode + "\n")
   })
-  res.send("bbox1")
-})
+});
 
 app.get('/bbox2', (_req, res) => {
   request.get('http://box:9091/').on('response', (response) => {
     console.log("bbox2 " + response.statusCode)
+    res.send("bbox2 " + response.statusCode + "\n")
   })
-  res.send("bbox2")
-})
+});
 
 app.get('/ping', (_req, res) => {
-  res.status(200).send('Ping');
-})
+  res.status(200).send('Ping\n');
+});
 
 app.get('/heavy', (_req, res) => {
   // Date() me devuelve milisegundos
   let start = new Date();
   while(new Date() - start < TIMEOUT);
-  res.status(200).send('Heavy work done')
+  res.status(200).send('Heavy work done\n')
 });
